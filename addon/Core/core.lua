69b3bb2a89b07eb7c5ec24f8f70665a8e6660a75
scizzydo <scottherder@outlook.com>
2022-01-15 10:56:00 -0700

    Started input hook part

--- addon/Core/core.lua
+++ addon/Core/core.lua
@@ -3,14 +3,28 @@ local ni = ...
 -- Prevent double loading (Shouldn't happen, but just to be safe)
 if not ni.loaded then
    local base_path = ni.backend.GetBaseFolder()
+   local core_path = base_path.."addon\\core\\"
 
    -- Load the utilities table for use
    do
-      local func, err = ni.backend.LoadFile(base_path.."addon\\core\\utilities.lua", "utilities.lua")
+      local func, err = ni.backend.LoadFile(core_path.."utilities.lua", "utilities.lua")
       if func then
          func(ni)
       else
          ni.backend.Error(err)
+      end
+   end
+
+   -- As long as the files isn't inserted/removed it'll stay in this order
+   local core_files = {
+      "inputhook.lua"
+   }
+
+   -- Load each of the above files here
+   for _, file in pairs(core_files) do
+      local _, error = ni.utilities.load_file(core_path..file, file)
+      if error then
+         ni.backend.Error(error)
       end
    end
 
