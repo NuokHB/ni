b8064c6c1e2bdcdbbf3e619ea9f5632131b43050
NuokHB <rsd1990@gmail.com>
2022-01-16 20:34:29 +0000

    type checking instead of to number formating boolean ;)

--- addon/Core/power.lua
+++ addon/Core/power.lua
@@ -1,5 +1,4 @@
 -------------------
-
 -- Power functions
 local ni = ...
 
@@ -7,8 +6,12 @@ local ni = ...
 local UnitPower = ni.utilities.get_function("UnitPower")
 local UnitPowerMax = ni.utilities.get_function("UnitPowerMax")
 
-local power = {}
-power.types = {
+ni.power = {}
+
+--[[--
+WoW Power types with there respective ingame value
+]]
+ni.power.power_types = {
    mana = 0,
    rage = 1,
    focus = 2,
@@ -32,19 +35,19 @@ Returns the power type value for a target as a percentage
  
 Parameters:
 - **target** `token|guid`
-- **type** `string`
-
+- **power_type** `string`
+ 
 Returns:
 - **current power** `number`
 @param target string
-@param type string
+@param power_type string
 ]]
-function ni.power.current(target, type)
-   if tonumber(type) == nil then
-      type = power.types[type]
+function ni.power.current_percent(target, power_type)
+   if type(power_type) == "string" then
+      power_type = ni.power.power_types[power_type]
    end
 
-   return 100 * UnitPower(target, type) / UnitPowerMax(target, type)
+   return 100 * UnitPower(target, power_type) / UnitPowerMax(target, power_type)
 end
 
 --[[--
@@ -52,19 +55,19 @@ Returns the power type value for a target as raw value
  
 Parameters:
 - **target** `token|guid`
-- **type** `string`
-
+- **power_type** `string`
+ 
 Returns:
 - **current power** `number`
 @param target string
-@param type string
+@param power_type string
 ]]
-function ni.power.current_raw(target, type)
-   if tonumber(type) == nil then
-      type = power.types[type]
+function ni.power.current(target, power_type)
+   if type(power_type) == "string" then
+      power_type = ni.power.power_types[power_type]
    end
 
-   return UnitPower(target, type)
+   return UnitPower(target, power_type)
 end
 
 --[[--
@@ -72,19 +75,19 @@ Returns the power type maximum value
  
 Parameters:
 - **target** `token|guid`
-- **type** `string`
-
+- **power_type** `string`
+ 
 Returns:
 - **max** `number`
 @param target string
-@param type string
+@param power_type string
 ]]
-function ni.power.max(target, type)
-   if tonumber(type) == nil then
-      type = power.types[type]
+function ni.power.max(target, power_type)
+   if type(power_type) == "string" then
+      power_type = ni.power.power_types[power_type]
    end
 
-   return UnitPowerMax(target, type)
+   return UnitPowerMax(target, power_type)
 end
 
 --[[--
@@ -93,16 +96,16 @@ Returns if the power type is at maximum or full
 Parameters:
 - **target** `token|guid`
 - **type** `string`
-
+ 
 Returns:
-- **ismax** `bool`
+- **ismax** `boolean`
 @param target string
 @param type string
 ]]
-function ni.power.ismax(target, type)
-   if tonumber(type) == nil then
-      type = power.types[type]
+function ni.power.is_max(target, power_type)
+   if type(power_type) == "string" then
+      power_type = ni.power.power_types[power_type]
    end
 
-   return UnitPower(target, type) == UnitPowerMax(target, type)
+   return UnitPower(target, power_type) == UnitPowerMax(target, power_type)
 end
