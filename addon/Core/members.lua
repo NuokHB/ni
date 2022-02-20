4596818f610ba2d9b697da34126410ddeef39e16
NuokHB <rsd1990@gmail.com>
2022-02-20 01:10:50 +0000

    Changed members to use metatables

--- addon/Core/members.lua
+++ addon/Core/members.lua
@@ -2,12 +2,13 @@
 -- Members functions for ni
 local ni = ...
 
+local setmetatable = ni.client.get_function("setmetatable")
+local rawset = ni.client.get_function("rawset")
+
 --[[--
 Table keys:
 - **guid** `string`
-- **name** `string`
 - **unit_id** `string`
-- **in_range** `boolean`
 @table members
 ]]
 ni.members = {}
@@ -16,27 +17,42 @@ ni.members = {}
 Updates the members table on call.
 ]]
 function ni.members.update()
-   for k, v in ni.table.opairs(ni.members) do
-      ni.members[k] = nil
-   end
+   ni.table.owipe(ni.members)
    local group = ni.group.in_raid() and "raid" or "party"
    for i=1, ni.group.size() do
       local unit_id = group..i
       local guid = ni.unit.guid(unit_id)
       ni.members[guid] = {
          guid = guid,
-         name = ni.unit.name(unit_id),
          unit_id = unit_id,
-         in_range = ni.group.in_range(unit_id, 40)
       }
+      setmetatable(ni.members[guid], {
+         __index = function(table, key)
+            if ni.unit[key] then
+               rawset(table, key, function(...)
+                  return ni.unit[key](unit_id, ...)
+               end)
+               return table[key]
+            end
+         end
+      })
    end
+   --Make sure we include the player in the table
    local player_guid = ni.player.guid()
    if not ni.table.contains_key(ni.members, player_guid) then
       ni.members[player_guid] = {
          guid = player_guid,
-         name = ni.unit.name(player_guid),
          unit_id = "player",
-         in_range = true
       }
+      setmetatable(ni.members[player_guid], {
+         __index = function(table, key)
+            if ni.unit[key] then
+               rawset(table, key, function(...)
+                  return ni.unit[key]("player", ...)
+               end)
+               return table[key]
+            end
+         end
+      })
    end
 end
