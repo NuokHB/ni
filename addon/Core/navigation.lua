4603288f5800e81fb00f9073d7285ad53f250020
NuokHB <rsd1990@gmail.com>
2022-06-13 09:45:06 +0100

    Added Documentation

--- addon/Core/navigation.lua
+++ addon/Core/navigation.lua
@@ -60,7 +60,27 @@ Parameters:
 function ni.navigation.move_to(x, y, z)
    return ni.backend.MoveTo(x, y, z)
 end
-
+   
+--[[--
+Calculates the distance between two 3D points
+    
+Parameters:
+- **x1** `number`
+- **y1** `number`
+- **z1** `number`
+- **x2** `number`
+- **y2** `number`
+- **z2** `number`
+    
+Returns:
+- **distance** `number`
+@param x1 number
+@param y1 number
+@param z1 number
+@param x2 number
+@param y2 number
+@param z2 number
+]]
 function ni.navigation.distanceV3(x1, y1, z1, x2, y2, z2)
    return math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2 + (z2 - z1) ^ 2)
 end
